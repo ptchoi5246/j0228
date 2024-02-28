85cb5e72460bfaea3d677b6c6f132d630bae281d
minimum <pt_choi5246@naver.com>
2024-02-28 21:22:49 +0900

    20240228_2122 'p

---
+++ src/t1_if/Test1_p.java
@@ -0,0 +1,50 @@
+package t1_if;
+
+import java.util.Scanner;
+
+// 두개의 수를 교환
+public class Test1_p {
+	public static void main(String[] args) {
+		
+		int su1, su2, temp;
+		
+		//수를 입력받는다
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("첫번째 숫자를 입력해주세요!  ");
+		su1 = sc.nextInt();
+		
+		System.out.print("두번째 숫자를 입력해주세요!  ");
+		su2 = sc.nextInt();
+		
+		System.out.println("첫번째 숫자 : "+su1+", 두번째 숫자 : "+su2+"");
+		
+		//두 수의 교환
+		
+		temp = su1;
+		su1 = su2;
+		su2 = temp;
+		
+		System.out.println("교환된 수 su1 : "+su1+", su2 " +su2);
+		
+		//temp를 통해서 수를 교환했기 때문에 su1, su2 그대로 입력
+	
+		sc.close();
+	}	
+}
+
+
+/*알고리즘 - 사진
+ * A = 100 B = 200
+ * B=A A=100, B=100 
+ * A=B A=100, B=100 :: 값을 덮어쓰게 된다.
+ * T 템프 =/ 값을 교환한다
+ * A = 100 B = 200 T 템프
+ * B -> T A->B T->A
+ * 
+ * T=B
+ * B=A
+ * A=T
+ * T=B; B=A; A=T;*/
+ 
--- src/t1_if/Test2_p.java
+++ src/t1_if/Test2_p.java
@@ -1,11 +1,11 @@
 package t1_if;
 
 // 논리 연산자 (!not, &&and, ||or)
-public class Test2{
+public class Test2_p {
 	public static void main(String[] args) {
 		int su1 = 20, su2 = 15, su3 = 12, max = -99;
 		
-		if(su1 > su2) {
+		if (su1 > su2) {
 			if (su1 > su3) {
 				max = su1;
 			}
@@ -14,7 +14,7 @@ public class Test2{
 			}
 		}
 		else {
-			if(su2 > su3) {
+			if (su2 > su3) {
 				max = su2;
 			}
 			else {
